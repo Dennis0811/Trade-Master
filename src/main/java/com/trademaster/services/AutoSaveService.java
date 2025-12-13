e1ff6b570aad2facac632cfc8f24ebea6dac717f
Dennis0811 <dennis.bauer96@gmx.de>
2025-12-13 11:32:03 +0100

    forgot to change to minutes

--- src/main/java/com/trademaster/services/AutoSaveService.java
+++ src/main/java/com/trademaster/services/AutoSaveService.java
@@ -55,7 +55,7 @@ public class AutoSaveService {
                 this::autoSave,
                 intervalMinutes,
                 intervalMinutes,
-                TimeUnit.SECONDS
+                TimeUnit.MINUTES
         );
     }
 
